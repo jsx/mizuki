d921495e7bba00d94ad25d8df9088861e5f8601c
Fuji, Goro <g.psy.va@gmail.com>
2012-07-21 14:34:27 +0900

    expandtabs

--- example/datetime.jsx
+++ example/datetime.jsx
@@ -1,17 +1,18 @@
 import "../lib/mizuki/datetime.jsx";
 
 class _Main {
-	static function main(args : string[]) : void {
-		var d = new Date;
+    static function main(args : string[]) : void {
+        var d = new Date;
 
-		if (args.length > 0) {
-			DateTime.setLocale(args[0]);
-		}
+        if (args.length > 0) {
+            DateTime.setLocale(args[0]);
+        }
 
-		var f = "[%a %b %d %Y %H:%M:%S.%3N GMT%z (%Z)]";
+        var f = "[%a %b %d %Y %H:%M:%S.%3N GMT%z (%Z)]";
 
-		var s = DateTime.strftime(d, f);
-		log "strftime: " + s;
-		log "strptime: " + DateTime.strftime(DateTime.strptime(s, f), f);
-	}
+        var s = DateTime.strftime(d, f);
+        log "strftime: " + s;
+        log "strptime: " + DateTime.strftime(DateTime.strptime(s, f), f);
+    }
 }
+// vim: set expandtab:
