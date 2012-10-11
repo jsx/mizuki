ec08ef95b9a58b06ce92e85f0445c08d7392a068
Fuji, Goro <g.psy.va@gmail.com>
2012-10-12 00:26:07 +0900

    performance improvement

--- lib/mizuki/base64.jsx
+++ lib/mizuki/base64.jsx
@@ -27,10 +27,9 @@ final class Base64 {
     static function _utob_cb(s : string) : string {
         var c = s.charCodeAt(0);
 
-        if (c < 0x80) {
-            return s;
-        }
-        else if (c < 0x800) {
+        assert c >= 0x80;
+
+        if (c < 0x800) {
             return String.fromCharCode(
                      (c >>>  6)         | 0xC0,
                      (c         & 0x3F) | 0x80);
@@ -44,7 +43,23 @@ final class Base64 {
     }
 
     static function _encode_utf8(str : string) : string {
-        return str.replace(/[^\x00-\x7F]/g, Base64._utob_cb);
+        return str.replace(/[^\x00-\x7F]/g, function (s) {
+            var c = s.charCodeAt(0);
+
+            assert c >= 0x80;
+
+            if (c < 0x800) {
+                return String.fromCharCode(
+                         (c >>>  6)         | 0xC0,
+                         (c         & 0x3F) | 0x80);
+            }
+            else {
+                return String.fromCharCode(
+                        ((c >>> 12) & 0x0F) | 0xE0,
+                        ((c >>>  6) & 0x3F) | 0x80,
+                         (c         & 0x3F) | 0x80);
+            }
+        });
     }
 
     static function _btoa(bin : string) : string {
@@ -55,25 +70,25 @@ final class Base64 {
             case 1:
                 var ord = (c.charCodeAt(0) << 16);
                 a += Base64._b64chars.charAt( ord >>> 18)
-                  + Base64._b64chars.charAt((ord >>> 12) & 63)
-                  + "==";
+                   + Base64._b64chars.charAt((ord >>> 12) & 63)
+                   + "==";
                 break;
             case 2:
                 var ord = (c.charCodeAt(0) << 16)
                         | (c.charCodeAt(1) <<  8);
                 a += Base64._b64chars.charAt( ord >>> 18)
-                  + Base64._b64chars.charAt((ord >>> 12) & 63)
-                  + Base64._b64chars.charAt((ord >>>  6) & 63)
-                  + "=";
+                   + Base64._b64chars.charAt((ord >>> 12) & 63)
+                   + Base64._b64chars.charAt((ord >>>  6) & 63)
+                   + "=";
                 break;
             default:
                 var ord = (c.charCodeAt(0) << 16)
                         | (c.charCodeAt(1) <<  8)
                         |  c.charCodeAt(2);
                 a += Base64._b64chars.charAt( ord >>> 18)
-                  + Base64._b64chars.charAt((ord >>> 12) & 63)
-                  + Base64._b64chars.charAt((ord >>>  6) & 63)
-                  + Base64._b64chars.charAt( ord         & 63);
+                   + Base64._b64chars.charAt((ord >>> 12) & 63)
+                   + Base64._b64chars.charAt((ord >>>  6) & 63)
+                   + Base64._b64chars.charAt( ord         & 63);
                 break;
             }
         }
@@ -97,21 +112,24 @@ final class Base64 {
     }
 
     static function _atob(str : string) : string {
-        return str.replace(/.{1,4}/g, function (x) {
-            var c = x.replace(/=/g, "");
+        var b = "";
+        for (var i = 0; i < str.length; i += 4) {
+            var c = str.slice(i, i+4).replace(/=/g, "");
             switch (c.length) {
             case 1:
                 var ord = (Base64._b64tab[c.charAt(0)] << 18);
-                return String.fromCharCode( ord >>> 16,
-                                           (ord >>>  8) & 0xFF,
-                                            ord         & 0xFF);
+                b += String.fromCharCode( ord >>> 16,
+                                         (ord >>>  8) & 0xFF,
+                                          ord         & 0xFF);
+                break;
             case 2:
                 var ord = (Base64._b64tab[c.charAt(0)] << 18)
                         | (Base64._b64tab[c.charAt(1)] << 12);
                 var s = String.fromCharCode( ord >>> 16,
                                             (ord >>>  8) & 0xFF,
                                              ord         & 0xFF);
-                return s.slice(0, 1);
+                b += s.slice(0, 1);
+                break;
             case 3:
                 var ord = (Base64._b64tab[c.charAt(0)] << 18)
                         | (Base64._b64tab[c.charAt(1)] << 12)
@@ -119,17 +137,20 @@ final class Base64 {
                 var s = String.fromCharCode( ord >>> 16,
                                             (ord >>>  8) & 0xFF,
                                              ord         & 0xFF);
-                return s.slice(0, 2);
+                b += s.slice(0, 2);
+                break;
             default:
                 var ord = (Base64._b64tab[c.charAt(0)] << 18)
                         | (Base64._b64tab[c.charAt(1)] << 12)
                         | (Base64._b64tab[c.charAt(2)] <<  6)
                         |  Base64._b64tab[c.charAt(3)];
-                return String.fromCharCode( ord >>> 16,
-                                           (ord >>>  8) & 0xFF,
-                                            ord         & 0xFF);
+                b += String.fromCharCode( ord >>> 16,
+                                         (ord >>>  8) & 0xFF,
+                                          ord         & 0xFF);
+                break;
             }
-        });
+        }
+        return b;
     }
 }
 
